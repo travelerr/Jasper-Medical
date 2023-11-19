0168d520ff9cdb1ef4bcff85196c7d9588f13561
Justin <foliofinance@gmail.com>
2023-11-19 17:17:58 -0500

    field validation

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
+import { useFormState } from "react-dom";
 
 export default function EditInvoiceForm({
   invoice,
