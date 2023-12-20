29ab323572b1daaaf825cbcf548d141d6a6734f7
Justin <foliofinance@gmail.com>
2023-12-20 17:28:23 -0500

    commit before schema overhaul

--- app/patient/(overview)/page.tsx
+++ app/patient/(overview)/page.tsx
@@ -19,15 +19,15 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
+          {/* <CardWrapper /> */}
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
+          {/* <RevenueChart /> */}
         </Suspense>
         <Suspense fallback={<InvoiceSkeleton />}>
-          <LatestInvoices />
+          {/* <LatestInvoices /> */}
         </Suspense>
       </div>
     </main>
