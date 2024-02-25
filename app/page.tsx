89511a20ac394f1537fd940a90e1b36dddf72cbe
Justin <foliofinance@gmail.com>
2024-02-24 19:04:53 -0500

    typo

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-3/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Jasper Med.</strong> <br />
-            We are in the process of simplifying pateint health for Direct
+            We are in the process of simplifying patient health for Direct
             Primary Care offices.
           </p>
           <p>
