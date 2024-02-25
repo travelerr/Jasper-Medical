31803e86d3327f9407239ac5bb3868cf6cda0e11
Justin <foliofinance@gmail.com>
2024-02-24 19:15:14 -0500

    update contact us

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -2,13 +2,11 @@ import JasperLogo from "@/app/_ui/shared/jasper-logo";
 
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div>
-          <a href="mailto:justin.sacco@jaspermed.com" className="link">
-            justin.sacco@jaspermed.com
-          </a>
-        </div>
+    <main className="flex items-center justify-center h-screen">
+      <div>
+        <a href="mailto:justin.sacco@jaspermed.com" className="link">
+          justin.sacco@jaspermed.com
+        </a>
       </div>
     </main>
   );
